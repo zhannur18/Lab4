1b2da85b781f44ae012a75fe5230440aadb8980b
Zhannur <221952@astanait.edu.kz>
2023-05-14 02:46:21 +0600

    private HashNode

---
+++ src/MyHashTable.java
@@ -0,0 +1,21 @@
+public class MyHashTable<K, V> {
+    private class HashNode<K, V> {
+        private K key;
+        private V value;
+        private HashNode<K, V> next;
+
+        public HashNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + key + " " + value + "}";
+        }
+    }
+    private HashNode<K, V>[] chainArray;
+    private int M = 11;
+    private int size;
+
+}
